b501ad6f513d475fb1df315ff7aa295d34319eed
sengsgx <61096585+sengsgx@users.noreply.github.com>
2020-04-30 23:32:00 +0200

    SDK Container: make sdk/psw reinstall the default option

--- seng_sdk/sdk_docker_bootstrap.bash
+++ seng_sdk/sdk_docker_bootstrap.bash
@@ -4,17 +4,23 @@ if [ ! -d "/opt/intel/" ]; then
 
     if [ -d ~/seng_sdk/external/linux-sgx-sgx_2.7.1/ ]; then
         echo "Note: SDK-PSW seems to have been downloaded"
-        read -p "Try re-install? (y/N) " choice
+        read -p "Try re-install? (Y/n) " choice
+        REINSTALL=false
         case $choice in
             [Yy]* )
-                pushd ~/seng_sdk/;
-                ./reinstall_patched_sdkpsw_sgxssl.bash;
-                popd;;
+                REINSTALL=true;
+                ;;
             [Nn]* )
                 ;;
             *)
+                REINSTALL=true;
                 ;;
         esac
+        if $REINSTALL; then
+            pushd ~/seng_sdk/;
+            ./reinstall_patched_sdkpsw_sgxssl.bash;
+            popd;
+        fi
     fi
 fi
 /bin/bash
